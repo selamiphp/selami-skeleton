157bac3d034231cb7b395a16f450b39632f676f4
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2017-06-17 18:44:00 +0300

    config organization changed.

--- config/autoload/app.global.php
+++ config/autoload/app.global.php
@@ -3,10 +3,6 @@ declare(strict_types=1);
 
 return [
     'app' => [
-        'templates_dir' => APP_DIR .'/templates',
-        'cache_dir' => '/tmp',
-        'debug' => false,
-        'auto_reload' => false,
-        'cache_file' => '/tmp/fastroute.cache'
+        'app_name' => 'Selami Skeleton App'
     ]
 ];

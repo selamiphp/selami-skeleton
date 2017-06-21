d769b517c3856890ae96a3979a3c98c119f6d31a
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2017-06-21 14:35:59 +0300

    server:run command has options for host and webroot directory

--- src/commands/Server/ServerRun.php
+++ src/commands/Server/ServerRun.php
@@ -5,6 +5,7 @@ namespace SelamiApp\Command\Server;
 
 use Selami\Console\Command as SelamiCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Exception\LogicException;
 use Symfony\Component\Console\Exception\InvalidArgumentException;
@@ -12,6 +13,8 @@ use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Exception\RuntimeException;
 use Symfony\Component\Process\Exception\InvalidArgumentException as ProcessInvalidArgumentException;
+
+
 class ServerRun extends SelamiCommand
 {
     /**
@@ -22,6 +25,8 @@ class ServerRun extends SelamiCommand
     {
         $this
             ->setName('server:run')
+            ->addOption('host',null, InputOption::VALUE_OPTIONAL, 'Hostname of the site', '127.0.0.1')
+            ->addOption('public',null, InputOption::VALUE_OPTIONAL, 'Public web root.', './public')
             ->setDescription('Run web server locally');
     }
 
@@ -31,12 +36,14 @@ class ServerRun extends SelamiCommand
      * @throws ProcessFailedException
      * @throws LogicException
      * @throws ProcessInvalidArgumentException
+     * @throws InvalidArgumentException
      */
     protected function execute(InputInterface $input, OutputInterface $output) : void
     {
-        $publicFolder = './public';
+        $hostName = $input->getOption('host');
+        $publicFolder = $input->getOption('public');
         $output->writeln('Starting Selami Skeleton App local web server from port 8080 on 127.0.0.1 at '.$publicFolder);
-        $process = new Process('php -S 127.0.0.1:8080 -t ' . $publicFolder);
+        $process = new Process('php -S ' . $hostName . ':8080 -t ' . $publicFolder);
         $process->setTimeout(null);
         $process->run();
         if (!$process->isSuccessful()) {
